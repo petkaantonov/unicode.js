925e8db3a006725eec0260b1600634bd2a7fcf2f
Petka Antonov <petka_antonov@hotmail.com>
2012-07-18 14:45:22 +0300

    Fix overlong sequence bug with utf-8

--- unicode.js
+++ unicode.js
@@ -671,29 +671,36 @@ Korean <-- multi-byte
     unicode.fromUTF8 = function( str ) {
         //Decode unicode code points from utf8 encoded string
         var codePoints = [],
-            i = 0, byte;
+            i = 0, byte, codePoint;
 
         while( !isNaN( byte = str.charCodeAt(i++) ) ) {
             if( (byte & 0xF8) === 0xF0 ) {
-                codePoints.push(
-                    ((byte & 0x7) << 18) |
-                    ((str.charCodeAt(i++) & 0x3F) << 12) |
-                    ((str.charCodeAt(i++) & 0x3F) << 6) |
-                    (str.charCodeAt(i++) & 0x3F)
-                );
+                codePoint = ((byte & 0x7) << 18) |
+                            ((str.charCodeAt(i++) & 0x3F) << 12) |
+                            ((str.charCodeAt(i++) & 0x3F) << 6) |
+                            (str.charCodeAt(i++) & 0x3F);
+                            
+                if( !( 0xFFFF < codePoint && codePoint <= 0x1FFFFF ) ) {
+                    codePoint = 0xFFFD;
+                }
+                codePoints.push(codePoint);
             }
             else if( (byte & 0xF0) === 0xE0 ) {
-                codePoints.push(
-                    ((byte & 0xF) << 12) |
-                    ((str.charCodeAt(i++) & 0x3F) << 6 ) |
-                    (str.charCodeAt(i++) & 0x3F)
-                );
+                codePoint = ((byte & 0xF) << 12) |
+                        ((str.charCodeAt(i++) & 0x3F) << 6 ) |
+                        (str.charCodeAt(i++) & 0x3F);
+                if( !( 0x7FF < codePoint && codePoint <= 0xFFFF ) ) {
+                    codePoint = 0xFFFD;
+                }
+                codePoints.push(codePoint);
             }
             else if( (byte & 0xE0) === 0xC0 ) {
-                codePoints.push(
-                    ((byte & 0x1F) << 6) |
-                    ( (str.charCodeAt(i++) & 0x3F) )
-                );
+                codePoint = ((byte & 0x1F) << 6) |
+                            ( (str.charCodeAt(i++) & 0x3F) );
+                if( !( 0x7F < codePoint && codePoint <= 0x7FF ) ) {
+                    codePoint = 0xFFFD;
+                }
+                codePoints.push(codePoint);
             }
             else if( (byte & 0x80) === 0x00 ) {
                 codePoints.push( byte & 0x7F );
